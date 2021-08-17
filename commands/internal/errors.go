8be59c52c15d122a3efd9b69f3453c85a18eb2da
oztasozgurcan <oztas.ozgurcan@gmail.com>
2021-08-17 15:36:40 +0300

    Removed unused error type.

--- commands/internal/errors.go
+++ commands/internal/errors.go
@@ -2,13 +2,8 @@ package internal
 
 import (
 	"errors"
-	"fmt"
 )
 
 var ErrMapKeyMissing = errors.New("map key is required")
 var ErrMapValueMissing = errors.New("map value is required")
 var ErrMapValueAndFileMutuallyExclusive = errors.New("only one of --value and --value-file must be specified")
-
-func RaiseErrAddressInvalid(addr string) error {
-	return fmt.Errorf("given address is invalid: %s", addr)
-}
