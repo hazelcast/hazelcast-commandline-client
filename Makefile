24a951f4334f7b8d2c29e6380aa5a922d0f50bf7
oztasozgurcan <oztas.ozgurcan@gmail.com>
2021-08-20 13:37:38 +0300

    Edited .goreleaser.yml.

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: install
-
-install:
-case $(echo $SHELL) in 
-	*/zsh) zsh install_zsh.zsh;;
-	*/bash) sh install_bash.sh;;
-esac
--- install_zsh.zsh
+++ install_zsh.zsh
@@ -1,5 +1,5 @@
-#!/usr/local/bin/zsh
+echo "Installation began."
 echo "sudo autoload -U compinit; compinit" >> ~/.zshrc
 cat completion_zsh.zsh > "${fpath[1]}/_hz-cli"
-sleep(1)
+sleep 1
 echo "Installation completed. For changes to take effect,\nclose the current terminal and reopen."
