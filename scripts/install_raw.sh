7f53753551255736db1ea9f6a699fa7e5ac88894
utkucaglayan <utku.caglayan@hazelcast.com>
2021-09-28 16:52:28 +0300

    fixes for zsh installation

--- scripts/install_raw.sh
+++ scripts/install_raw.sh
@@ -1,12 +1,11 @@
+#!/usr/bin/env bash
 
+PROGRAM_NAME="hz-cli"
+HZCLI_HOME="$HOME/.local/share/hz-cli"
 read -rd '' bashrcAddition << EOF
 for bcfile in \$HOME/.bash_completion.d/* ; do
     [ -f "\$bcfile" ] && . "\$bcfile"
 done
-EOF
-
-read -rd '' zshrcAddition << EOF
-echo "export PATH=\$HOME/.local/bin:\$PATH" >> \$HOME/.zshrc
 EOF
 
 ghExtractTag() {
@@ -35,26 +34,46 @@ esac
 tag=$(ghExtractTag hazelcast/hazelcast-commandline-client)
 releaseUrl=$(printf "https://github.com/hazelcast/hazelcast-commandline-client/releases/download/v%s/hz-cli_%s_%s" "$tag" "$tag" "$bin_id")
 
-curl -L --silent "$releaseUrl" --output "hz-cli"
-chmod +x "./hz-cli"
+curl -L --silent "$releaseUrl" --output "$HOME/$PROGRAM_NAME"
+chmod +x $HOME/$PROGRAM_NAME
 
 mkdir -p $HOME/.local/bin
-mv ./hz-cli $HOME/.local/bin/hz-cli
+mv $HOME/hz-cli $HOME/.local/bin
+echo "Hazelcast Commandline Client (CLC) is downloaded to \$HOME/.local/bin/$PROGRAM_NAME"
+echo
+
+
+read -rd '' addToPathDirectives << EOF
+*Add \$HOME/.local/bin to PATH to access hz-cli from any directory
+- To add it to your path, simply execute:
+> echo "export PATH=\$HOME/.local/bin:\$PATH" >> \$HOME/.zshrc
+
+EOF
+
+read -rd '' zshAutocompletionDirectives << EOF
+* To enable autocompletion capability for Zsh if you have not already
+- Append the line below in your .zshrc file if it doesn't exists
+> autoload -U compinit; compinit
+
+* Enable autocompletion for Hazelcast Commandline Client (CLC)
+- Create a symbolic link of autocompletion script to one of your paths in your fpath such as
+> sudo ln -s $HZCLI_HOME/autocompletion/zsh/hz-cli \${fpath[1]}/_hz-cli
+
+* Restart your terminal for the CLC autocompletion to take effect
+or renew your session via:
+> /usr/bin/zsh
+
+EOF
 
 case "$(printf "${SHELL##*bin\/}")" in
     "zsh")
-        if [[ ! "$(cat $HOME/.zshrc)" == *"$(echo "export PATH=$HOME/.local/bin:$PATH")"* ]]; then
-            echo "add $HOME/.local/bin to PATH to access hz-cli from any directory"
-            echo "open your .zshrc file and at the end, add the line below:"
-            echo "$zshrcAddition"
+        if [[ ! -r $HOME/.zshrc || ! "$(cat $HOME/.zshrc)" == *"$(echo "export PATH=$HOME/.local/bin:$PATH")"* ]]; then
+            echo "$addToPathDirectives"
         fi
-        completionUrl=$(printf "https://raw.githubusercontent.com/hazelcast/hazelcast-commandline-client/main/extras/%s" "zsh_completion.sh")
-        curl --silent "$completionUrl" --output $HOME/.zsh_completion.sh
-        if [[ ! "$(cat $HOME/.zshrc)" == *"$(echo "autoload -U compinit; compinit")"* ]]; then
-            echo "autoload -U compinit; compinit" >> $HOME/.zshrc
-        fi
-        cat $HOME/.zsh_completion.sh > "${fpath[1]}/_hz-cli"
-        rm -rf $HOME/.zsh_completion.sh
+        curl --silent "https://raw.githubusercontent.com/hazelcast/hazelcast-commandline-client/main/extras/zsh_completion.zsh" --output $HOME/.zsh_completion.sh
+        mkdir -p $HZCLI_HOME/autocompletion/zsh
+        mv $HOME/.zsh_completion.sh $HZCLI_HOME/autocompletion/zsh/hz-cli
+        echo "$zshAutocompletionDirectives"
     ;;
     "bash")
         if [[ ! "$(cat $HOME/.bashrc)" == *"$(echo "export PATH=$HOME/.local/bin:$PATH")"* ]]; then
@@ -68,14 +87,5 @@ case "$(printf "${SHELL##*bin\/}")" in
         if [[ ! "$(cat $HOME/.bashrc)" == *"$(echo "$bashrcAddition")"* ]]; then
             echo "$bashrcAddition" >> $HOME/.bashrc
         fi
-    ;;
-esac
-
-case "$(printf "${SHELL##*bin\/}")" in
-    "zsh")
-        /bin/zsh;
-    ;;
-    "bash")
-        /bin/bash;
     ;;
 esac
