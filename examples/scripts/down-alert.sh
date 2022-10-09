afbb803caa403488426644bab82c45fcd6f7e868
Yuce Tekol <yuce.tekol@hazelcast.com>
2022-10-09 22:41:50 +0300

    updates

--- examples/scripts/down-alert.sh
+++ examples/scripts/down-alert.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
+set -e
+set -u
+
+path="${1:-/tmp/warnings.txt}"
+
 while true; do
   read -r line
-  state=$(echo $line | cut -f1 -d' ')
-  if [ "$state" == "REM" ]; then
-    addr=$(echo $line | cut -f2 -d' ')
-    uuid=$(echo $line | cut -f3 -d' ')
-    echo "WARNING: Member $uuid at address $addr was removed from the cluster" | tee -a /tmp/warnings.txt
+  state=$(echo $line | cut -f3 -d' ')
+  echo "STATE: $state"
+  if [ "$state" == "CLIENT_DISCONNECTED" ]; then
+    echo "WARNING: Client disconnected" | tee -a "$path"
+  elif [ "$state" == "REMOVED" ]; then
+    addr=$(echo $line | cut -f4 -d' ')
+    uuid=$(echo $line | cut -f6 -d' ')
+    echo "WARNING: Member $uuid at address $addr was removed from the cluster" | tee -a "$path"
   fi
 done
