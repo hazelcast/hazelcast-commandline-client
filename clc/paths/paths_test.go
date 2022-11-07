8b93697886f93b70a5932dda90559e8d4d075f35
Yuce Tekol <yuce.tekol@hazelcast.com>
2022-11-07 13:35:01 +0300

    Added more tests

--- clc/paths/paths_test.go
+++ clc/paths/paths_test.go
@@ -1,6 +1,7 @@
 package paths_test
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -49,6 +50,58 @@ func TestDefaultLogPath_Windows(t *testing.T) {
 	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
 	now := time.Date(2020, 2, 1, 9, 0, 0, 0, time.UTC)
 	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\logs\2020-02-01.log`, paths.DefaultLogPath(now))
+}
+
+func TestSchemas_Unix(t *testing.T) {
+	skip.If(t, "os = windows")
+	Must(os.Setenv("HOME", "/dev/shm"))
+	assert.Equal(t, "/dev/shm/.local/share/clc/schemas", paths.Schemas())
+}
+
+func TestSchemas_Windows(t *testing.T) {
+	skip.IfNot(t, "os = windows")
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\schemas`, paths.Schemas())
+}
+
+func TestResolveConfigPath_Unix(t *testing.T) {
+	skip.If(t, "os = windows")
+	Must(os.Setenv("HOME", "/dev/shm"))
+	// default config
+	assert.Equal(t, "/dev/shm/.local/share/clc/configs/default/config.yaml", paths.ResolveConfigPath(""))
+	// path to the configuration file
+	assert.Equal(t, "/etc/hz.yaml", paths.ResolveConfigPath("/etc/hz.yaml"))
+	// configuration name
+	assert.Equal(t, "/dev/shm/.local/share/clc/configs/pr-3066/config.yaml", paths.ResolveConfigPath("pr-3066"))
+}
+
+func TestResolveConfigPath_Windows(t *testing.T) {
+	skip.IfNot(t, "os = windows")
+	// default config
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\configs\default\config.yaml`, paths.ResolveConfigPath(""))
+	// path to the configuration file
+	assert.Equal(t, `C:\hz.yaml`, paths.ResolveConfigPath(`C:\hz.yaml`))
+	// configuration name
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\configs\pr-3066\config.yaml`, paths.ResolveConfigPath("pr-3066"))
+}
+
+func TestResolveLogPath_Unix(t *testing.T) {
+	skip.If(t, "os = windows")
+	Must(os.Setenv("HOME", "/dev/shm"))
+	now := time.Now()
+	path := fmt.Sprintf("/dev/shm/.local/share/clc/logs/%s.log", now.Format("2006-01-02"))
+	assert.Equal(t, path, paths.ResolveLogPath(""))
+	assert.Equal(t, "/var/hz.log", paths.ResolveLogPath("/var/hz.log"))
+}
+
+func TestResolveLogPath_Windows(t *testing.T) {
+	skip.IfNot(t, "os = windows")
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
+	now := time.Now()
+	path := fmt.Sprintf(`C:\Users\foo\AppData\Roaming\Hazelcast CLC\logs\%s.log`, now.Format("2006-01-02"))
+	assert.Equal(t, path, paths.ResolveLogPath(""))
+	assert.Equal(t, `C:\hz.log`, paths.ResolveLogPath(`C:\hz.log`))
 }
 
 func TestJoin(t *testing.T) {
