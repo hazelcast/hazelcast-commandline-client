26b28eddc4ec79d40cc542621ae41ea2b666999c
Yuce Tekol <yuce.tekol@hazelcast.com>
2022-11-07 10:16:09 +0300

    Updated Win path tests

--- clc/paths/paths_test.go
+++ clc/paths/paths_test.go
@@ -21,8 +21,8 @@ func TestHomeDir_Unix(t *testing.T) {
 
 func TestHomeDir_Windows(t *testing.T) {
 	skip.IfNot(t, "os = windows")
-	Must(os.Setenv("USERPROFILE", `C:/Users/foo`))
-	assert.Equal(t, `C:/Users/foo/AppData/Roaming/Hazelcast CLC`, paths.Home())
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC`, paths.Home())
 }
 
 func TestDefaultConfigPath_Unix(t *testing.T) {
@@ -33,8 +33,8 @@ func TestDefaultConfigPath_Unix(t *testing.T) {
 
 func TestDefaultConfigPath_Windows(t *testing.T) {
 	skip.IfNot(t, "os = windows")
-	Must(os.Setenv("USERPROFILE", `C:/Users/foo`))
-	assert.Equal(t, `C:/Users/foo/AppData/Roaming/Hazelcast CLC/configs/default/config.yaml`, paths.Home())
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\configs\default\config.yaml`, paths.DefaultConfigPath())
 }
 
 func TestDefaultLogPath_Unix(t *testing.T) {
@@ -46,12 +46,13 @@ func TestDefaultLogPath_Unix(t *testing.T) {
 
 func TestDefaultLogPath_Windows(t *testing.T) {
 	skip.IfNot(t, "os = windows")
-	Must(os.Setenv("USERPROFILE", `C:/Users/foo`))
+	Must(os.Setenv("USERPROFILE", `C:\Users\foo`))
 	now := time.Date(2020, 2, 1, 9, 0, 0, 0, time.UTC)
-	assert.Equal(t, "C:/Users/foo/AppData/Roaming/Hazelcast CLC/logs/2020-02-01.log", paths.DefaultLogPath(now))
+	assert.Equal(t, `C:\Users\foo\AppData\Roaming\Hazelcast CLC\logs\2020-02-01.log`, paths.DefaultLogPath(now))
 }
 
 func TestJoin(t *testing.T) {
+	skip.If(t, "os = windows")
 	testCases := []struct {
 		paths  []string
 		result string
