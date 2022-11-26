df1d7f7e416f4b5de4b58afefbb76cf4ea724ec2
Yuce Tekol <yuce.tekol@hazelcast.com>
2022-11-26 21:34:12 +0300

    win updates

--- make.cmd
+++ make.cmd
@@ -3,7 +3,8 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list --tags --max-count=1`) DO (
 set GIT_COMMIT=%%F
 )
 if not defined CLC_VERSION set CLC_VERSION=UNKNOWN
-set ldflags="-X 'github.com/hazelcast/hazelcast-go-client/internal.ClientType=CLC' -X 'github.com/hazelcast/hazelcast-commandline-client/internal.GitCommit=%GIT_COMMIT%' -X 'github.com/hazelcast/hazelcast-commandline-client/internal.ClientVersion=%CLC_VERSION%' -X 'github.com/hazelcast/hazelcast-go-client/internal.ClientVersion=%CLC_VERSION%'"
+echo "CLC_VERSION:" %CLC_VERSION%
+set ldflags="-X 'github.com/hazelcast/hazelcast-commandline-client/internal.GitCommit=%GIT_COMMIT%' -X 'github.com/hazelcast/hazelcast-commandline-client/internal.Version=%CLC_VERSION%' -X 'github.com/hazelcast/hazelcast-go-client/internal.ClientType=CLC' -X 'github.com/hazelcast/hazelcast-go-client/internal.ClientVersion=%CLC_VERSION%'"
 
 REM default target is build
 if "%1" == "" (
