9e703cb6e7a2eb27bf8b7ade6b41599368854def
kmetin <kutluhan.metin@hazelcast.com>
2023-05-24 14:53:10 +0300

    * add autoYes to map and queue clear commands * add queue destroy command

--- base/commands/queue/queue_clear.go
+++ base/commands/queue/queue_clear.go
@@ -7,8 +7,10 @@ import (
 	"fmt"
 
 	"github.com/hazelcast/hazelcast-commandline-client/clc"
+	"github.com/hazelcast/hazelcast-commandline-client/errors"
 	. "github.com/hazelcast/hazelcast-commandline-client/internal/check"
 	"github.com/hazelcast/hazelcast-commandline-client/internal/plug"
+	"github.com/hazelcast/hazelcast-commandline-client/internal/prompt"
 	"github.com/hazelcast/hazelcast-go-client"
 )
 
@@ -17,6 +19,7 @@ type QueueClearCommand struct{}
 func (qc *QueueClearCommand) Init(cc plug.InitContext) error {
 	help := "Delete all entries of a Queue"
 	cc.SetCommandHelp(help, help)
+	cc.AddBoolFlag(clc.FlagAutoYes, "", false, false, "skip confirming the clear operation")
 	cc.SetCommandUsage("clear")
 	return nil
 }
@@ -25,6 +28,18 @@ func (qc *QueueClearCommand) Exec(ctx context.Context, ec plug.ExecContext) erro
 	qv, err := ec.Props().GetBlocking(queuePropertyName)
 	if err != nil {
 		return err
+	}
+	autoYes := ec.Props().GetBool(clc.FlagAutoYes)
+	if !autoYes {
+		p := prompt.New(ec.Stdin(), ec.Stdout())
+		yes, err := p.YesNo("Queue content will be deleted irreversibly, proceed?")
+		if err != nil {
+			ec.Logger().Info("User input could not be processed due to error: %s", err.Error())
+			return errors.ErrUserCancelled
+		}
+		if !yes {
+			return errors.ErrUserCancelled
+		}
 	}
 	q := qv.(*hazelcast.Queue)
 	_, stop, err := ec.ExecuteBlocking(ctx, func(ctx context.Context, sp clc.Spinner) (any, error) {
