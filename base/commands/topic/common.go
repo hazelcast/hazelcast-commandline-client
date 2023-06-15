48217fe0c48ddb5424efafd76ea03655cf7fe327
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2023-06-15 12:01:57 +0300

    Change message

--- base/commands/topic/common.go
+++ base/commands/topic/common.go
@@ -41,7 +41,7 @@ func updateOutput(ctx context.Context, ec plug.ExecContext, events <-chan topic.
 	rowCh := make(chan output.Row)
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
 	defer stop()
-	ec.PrintlnUnnecessary(fmt.Sprintf("Listening for messages for topic %s", ec.Props().GetString(topicFlagName)))
+	ec.PrintlnUnnecessary(fmt.Sprintf("Listening for messages of topic %s", ec.Props().GetString(topicFlagName)))
 	go func() {
 	loop:
 		for {
