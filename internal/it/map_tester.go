063df3ed7436d51227781d932638ef4b295cbeb8
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2023-06-16 14:25:46 +0300

    Add map load-all command [CLC-178] (#263)
    Co-authored-by: Yüce Tekol <yucetekol@gmail.com>

--- internal/it/map_tester.go
+++ internal/it/map_tester.go
@@ -9,18 +9,25 @@ import (
 	"github.com/hazelcast/hazelcast-commandline-client/internal/check"
 )
 
-func WithMap(tcx TestContext, fn func(m *hz.Map)) {
-	name := NewUniqueObjectName("map")
+func WithNamedMap(tcx TestContext, mn string, fn func(m *hz.Map)) {
 	ctx := context.Background()
-	m := check.MustValue(tcx.Client.GetMap(ctx, name))
+	m := check.MustValue(tcx.Client.GetMap(ctx, mn))
 	fn(m)
 }
 
-func MapTester(t *testing.T, fn func(tcx TestContext, m *hz.Map)) {
+func WithMap(tcx TestContext, fn func(m *hz.Map)) {
+	WithNamedMap(tcx, NewUniqueObjectName("map"), fn)
+}
+
+func MapTesterWithName(t *testing.T, mn string, fn func(tcx TestContext, m *hz.Map)) {
 	tcx := TestContext{T: t}
 	tcx.Tester(func(tcx TestContext) {
-		WithMap(tcx, func(m *hz.Map) {
+		WithNamedMap(tcx, mn, func(m *hz.Map) {
 			fn(tcx, m)
 		})
 	})
+}
+
+func MapTester(t *testing.T, fn func(tcx TestContext, m *hz.Map)) {
+	MapTesterWithName(t, NewUniqueObjectName("map"), fn)
 }
