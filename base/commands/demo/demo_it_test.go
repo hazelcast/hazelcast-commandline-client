a6fcc1b7ffaaa74759d030399ffc9a477cb5fb34
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2023-06-21 14:54:26 +0300

    Fix generate-data tests

--- base/commands/demo/demo_it_test.go
+++ base/commands/demo/demo_it_test.go
@@ -2,6 +2,7 @@ package demo_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	hz "github.com/hazelcast/hazelcast-go-client"
@@ -18,6 +19,7 @@ func TestGenerateData(t *testing.T) {
 		f    func(t *testing.T)
 	}{
 		{name: "generateData_Wikipedia", f: generateData_WikipediaTest},
+		{name: "generateData_Wikipedia_MaxValues", f: generateData_Wikipedia_MaxValues_Test},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, tc.f)
@@ -29,10 +31,23 @@ func generateData_WikipediaTest(t *testing.T) {
 		t := tcx.T
 		ctx := context.Background()
 		tcx.WithReset(func() {
-			err := tcx.CLCExecuteErr(ctx, "demo", "generate-data", "wikipedia-event-stream", "map-name="+m.Name(), "--preview", "--timeout", "2s")
+			err := tcx.CLCExecuteErr(ctx, "demo", "generate-data", "wikipedia-event-stream", "map-name="+m.Name(), "--timeout", "2s")
 			require.Error(t, err)
 			size := check.MustValue(m.Size(context.Background()))
 			require.Greater(t, size, 0)
+		})
+	})
+}
+
+func generateData_Wikipedia_MaxValues_Test(t *testing.T) {
+	it.MapTester(t, func(tcx it.TestContext, m *hz.Map) {
+		t := tcx.T
+		ctx := context.Background()
+		count := 10
+		tcx.WithReset(func() {
+			tcx.CLCExecute(ctx, "demo", "generate-data", "wikipedia-event-stream", "map-name="+m.Name(), fmt.Sprintf("--max-values=%d", count))
+			size := check.MustValue(m.Size(context.Background()))
+			require.Equal(t, count, size)
 		})
 	})
 }
