ecef70a8ac2cb5205ccc98ea75314f8889f2dbee
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2023-06-21 10:17:08 +0300

    Add atomic-long command [CLC-191] (#280)
    Co-authored-by: Yüce Tekol <yucetekol@gmail.com>

--- cmd/clc/imports.go
+++ cmd/clc/imports.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands"
+	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/atomic_long"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/config"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/job"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/map"
