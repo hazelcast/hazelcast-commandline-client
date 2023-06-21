7c1c921f60c615549fdd4039dc4984c7d282a915
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2023-06-21 10:40:54 +0300

    Add list commands [CLC-192] (#283)
    Co-authored-by: Kutluhan Metin <kutluhanmetin@rocketmail.com>
    Co-authored-by: Yüce Tekol <yucetekol@gmail.com>

--- cmd/clc/imports.go
+++ cmd/clc/imports.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/atomic_long"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/config"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/job"
+	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/list"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/map"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/object"
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/set"
