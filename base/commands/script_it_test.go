684dcb4ec8e464bf059cc92da31906e9de744c67
Yuce Tekol <yuce.tekol@hazelcast.com>
2023-07-03 09:13:30 +0300

    Added interactive test

--- base/commands/script_it_test.go
+++ base/commands/script_it_test.go
@@ -2,9 +2,8 @@ package commands_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
-
-	"github.com/hazelcast/hazelcast-go-client"
 
 	_ "github.com/hazelcast/hazelcast-commandline-client/base/commands/map"
 	"github.com/hazelcast/hazelcast-commandline-client/internal/it"
@@ -15,6 +14,7 @@ func TestScript(t *testing.T) {
 		name string
 		f    func(t *testing.T)
 	}{
+		{name: "script_Interactive", f: script_InteractiveTest},
 		{name: "script_NonInteractive", f: script_NonInteractiveTest},
 	}
 	for _, tc := range testCases {
@@ -24,9 +24,24 @@ func TestScript(t *testing.T) {
 
 func script_NonInteractiveTest(t *testing.T) {
 	ctx := context.TODO()
-	it.MapTester(t, func(tcx it.TestContext, m *hazelcast.Map) {
+	tcx := it.TestContext{T: t}
+	tcx.Tester(func(tcx it.TestContext) {
 		tcx.CLCExecute(ctx, "script", "testdata/test-script.clc", "--echo", "--ignore-errors")
 		tcx.AssertStdoutContains("bar")
 		tcx.AssertStderrContains("unknown command")
+	})
+}
+
+func script_InteractiveTest(t *testing.T) {
+	ctx := context.TODO()
+	tcx := it.TestContext{T: t}
+	tcx.Tester(func(tcx it.TestContext) {
+		tcx.WithShell(ctx, func(tcx it.TestContext) {
+			tcx.WithReset(func() {
+				tcx.WriteStdinString(fmt.Sprintf("\\script testdata/test-script.clc --echo --ignore-errors\n"))
+				tcx.AssertStdoutContains("bar")
+				tcx.AssertStderrContains("unknown command")
+			})
+		})
 	})
 }
