ac5a906e25afdbb22dad1ebf035683c9b0263155
kmetin <kutluhan.metin@hazelcast.com>
2023-07-25 08:25:18 +0300

    refactor

--- base/commands/project/utils.go
+++ base/commands/project/utils.go
@@ -14,17 +14,14 @@ import (
 func loadFromDefaults(templateDir string) (map[string]string, error) {
 	props := make(map[string]string)
 	path := paths.Join(templateDir, defaultsFileName)
-	if !paths.Exists(path) {
-		return nil, nil
-	}
 	b, err := os.ReadFile(path)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			return nil, nil
+			return props, nil
 		}
 		return nil, err
 	}
-	if err = parseYAML("", b, &props); err != nil {
+	if err = parseYAML("", b, props); err != nil {
 		return nil, err
 	}
 	props = camelizeMapKeys(props)
@@ -61,7 +58,7 @@ func updatePropsWithUserInput(ec plug.ExecContext, props map[string]string) erro
 
 func loadFromProps(ec plug.ExecContext, p map[string]string) {
 	m := ec.Props().All()
-	maybeCamelizeMapKeys(m)
+	m = maybeCamelizeMapKeys(m)
 	for k, v := range m {
 		p[k] = fmt.Sprintf("%v", v)
 	}
@@ -75,7 +72,7 @@ func maybeCamelizeMapKeys(m map[string]any) map[string]any {
 	return r
 }
 
-func parseYAML(prefix string, yamlFile []byte, result *map[string]string) error {
+func parseYAML(prefix string, yamlFile []byte, result map[string]string) error {
 	var parsedData map[string]any
 	err := yaml.Unmarshal(yamlFile, &parsedData)
 	if err != nil {
@@ -88,10 +85,10 @@ func parseYAML(prefix string, yamlFile []byte, result *map[string]string) error 
 		fullKey := joinKeys(prefix, k)
 		switch val := v.(type) {
 		case string:
-			(*result)[fullKey] = val
+			(result)[fullKey] = val
 		default:
 			if _, isMap := val.(map[any]any); !isMap {
-				(*result)[fullKey] = fmt.Sprintf("%v", val)
+				(result)[fullKey] = fmt.Sprintf("%v", val)
 			}
 		}
 		if subMap, isMap := v.(map[any]any); isMap {
