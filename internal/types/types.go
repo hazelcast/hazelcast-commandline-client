483c25ef4849cd383412441e107d8dd42d2d4949
Yuce Tekol <yuce.tekol@hazelcast.com>
2023-07-28 14:21:51 +0300

    - Use Python code sample instead of Go - Fixed a couple of issues

--- internal/types/types.go
+++ internal/types/types.go
@@ -4,3 +4,10 @@ type Tuple2[A, B any] struct {
 	First  A
 	Second B
 }
+
+func MakeTuple2[A, B any](first A, second B) Tuple2[A, B] {
+	return Tuple2[A, B]{
+		First:  first,
+		Second: second,
+	}
+}
