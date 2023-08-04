2082f958ec38fa20954e0415895bfa69fcbde9c4
Yüce Tekol <yuce.tekol@hazelcast.com>
2023-08-04 15:50:53 +0300

    Fix loading access token when the API key is given explicitly (#325)
    Fixes getting the access token path when --api-key or CLC_VIRIDIAN_API_KEY env var is used

--- base/commands/viridian/common_test.go
+++ base/commands/viridian/common_test.go
@@ -22,19 +22,19 @@ func TestFindToken(t *testing.T) {
 	it.WithEnv(paths.EnvCLCHome, home.Path(), func() {
 		it.WithEnv(viridian.EnvAPIKey, "", func() {
 			// should return an error if there are no secrets
-			_, err := findToken("")
+			_, err := findTokenPath("")
 			require.Error(t, err)
 			// fixture
 			check.Must(secrets.Write(prefix, "api-APIKEY1.access", []byte("token-APIKEY1")))
 			check.Must(secrets.Write(prefix, "api-APIKEY2.access", []byte("token-APIKEY2")))
 			check.Must(secrets.Write(prefix, "cls-CLSKEY1.access", []byte("token-CLSKEY1")))
 			// check the token filename for the first API key is returned if the API key was not specified
-			require.Equal(t, "api-APIKEY1.access", check.MustValue(findToken("")))
+			require.Equal(t, "api-APIKEY1.access", check.MustValue(findTokenPath("")))
 			// check the token filename for the given API key is returned
-			require.Equal(t, "api-APIKEY2.access", check.MustValue(findToken("APIKEY2.access")))
+			require.Equal(t, "api-APIKEY2.access", check.MustValue(findTokenPath("APIKEY2")))
 			// check the token filename for the given API class is returned
 			it.WithEnv(viridian.EnvAPI, "cls", func() {
-				require.Equal(t, "cls-CLSKEY1.access", check.MustValue(findToken("CLSKEY1.access")))
+				require.Equal(t, "cls-CLSKEY1.access", check.MustValue(findTokenPath("CLSKEY1")))
 			})
 		})
 	})
