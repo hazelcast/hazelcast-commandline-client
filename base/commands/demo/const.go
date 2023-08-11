f6d558675840913fd8e5edcac17b6584b0f4762d
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2023-08-11 11:37:34 +0300

    Remove unused constant

--- base/commands/demo/const.go
+++ base/commands/demo/const.go
@@ -3,9 +3,8 @@
 package demo
 
 const (
-	GroupDemoID         = "demo"
-	flagPreview         = "preview"
-	flagMaxValues       = "max-values"
-	demoMapPropertyName = "demo-map"
-	pairMapName         = "map"
+	GroupDemoID   = "demo"
+	flagPreview   = "preview"
+	flagMaxValues = "max-values"
+	pairMapName   = "map"
 )
