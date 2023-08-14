c1a444906e9103f4a8702bc23371a33ee5e0a9a7
kmetin <kutluhan.metin@hazelcast.com>
2023-08-14 17:32:02 +0300

    fix test

--- clc/shell/common.go
+++ clc/shell/common.go
@@ -93,6 +93,8 @@ func ConvertStatement(ctx context.Context, ec plug.ExecContext, stmt string, ver
 func InteractiveHelp() string {
 	return `
 Shortcut Commands:
+	\di			  List Indexes
+	\di	 MAPPING  List Indexes for a specific mapping
 	\dm           List mappings
 	\dm  MAPPING  Display information about a mapping
 	\dm+ MAPPING  Describe a mapping
