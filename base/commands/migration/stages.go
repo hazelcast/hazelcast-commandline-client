3c215f883ad8f1876fb7f08bbcddc5c9f931bdd6
kmetin <kutluhan.metin@hazelcast.com>
2023-08-29 16:23:04 +0300

    fix PR comments

--- base/commands/migration/stages.go
+++ base/commands/migration/stages.go
@@ -15,6 +15,13 @@ import (
 	"github.com/hazelcast/hazelcast-commandline-client/internal/plug"
 )
 
+var statusErrMapping = map[status]error{
+	statusInProgress: nil,
+	statusComplete:   nil,
+	statusCanceled:   clcerrors.ErrUserCancelled,
+	statusFailed:     errors.New("migration failed"),
+}
+
 type Stages struct {
 	migrationID string
 	configDir   string
@@ -76,7 +83,7 @@ func (st *Stages) connectStage(ctx context.Context, ec plug.ExecContext) func(st
 }
 
 func (st *Stages) startStage(ctx context.Context) func(stage.Statuser) error {
-	return func(status stage.Statuser) error {
+	return func(stage.Statuser) error {
 		if err := st.statusMap.Delete(ctx, statusMapEntryName); err != nil {
 			return err
 		}
@@ -89,10 +96,10 @@ func (st *Stages) startStage(ctx context.Context) func(stage.Statuser) error {
 		if err != nil {
 			return err
 		}
-		if err := st.startQueue.Put(ctx, serialization.JSON(b)); err != nil {
+		if err = st.startQueue.Put(ctx, serialization.JSON(b)); err != nil {
 			return err
 		}
-		if err = st.waitStatusResult(ctx, 10, 1*time.Second); err != nil {
+		if err = st.waitForStatus(ctx, 30*time.Second, []status{statusInProgress, statusComplete}); err != nil {
 			return err
 		}
 		return nil
@@ -100,36 +107,43 @@ func (st *Stages) startStage(ctx context.Context) func(stage.Statuser) error {
 }
 
 func (st *Stages) migrateStage(ctx context.Context) func(statuser stage.Statuser) error {
-	return func(status stage.Statuser) error {
-		return st.waitStatusResult(ctx, -1, 5*time.Second)
+	return func(stage.Statuser) error {
+		return st.waitForStatus(ctx, 0, []status{statusComplete})
 	}
 }
 
-func (st *Stages) waitStatusResult(ctx context.Context, maxRetries int, period time.Duration) error {
-	tryInfinitely := maxRetries < 0
-	tryCount := 0
+func (st *Stages) waitForStatus(ctx context.Context, timeout time.Duration, expected []status) error {
+	if timeout != 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, timeout)
+		defer cancel()
+	}
 	for {
-		if !tryInfinitely {
-			if maxRetries == tryCount {
-				break
-			}
-			tryCount += 1
+		if err := ctx.Err(); err != nil && errors.Is(err, context.DeadlineExceeded) {
+			return fmt.Errorf("migration could not be completed: reached timeout while reading status: " +
+				"please ensure that you are using Hazelcast's migration cluster distribution and your DMT config points to that cluster")
 		}
 		s, err := st.readStatus(ctx)
 		if err != nil {
 			return fmt.Errorf("reading status: %w", err)
 		}
-		switch s {
-		case statusInProgress, statusComplete:
-			return nil
-		case statusCanceled:
-			return clcerrors.ErrUserCancelled
-		case statusFailed:
-			return errors.New("migration failed")
+		if statusErrMapping[s] != nil {
+			return statusErrMapping[s]
 		}
-		time.Sleep(period)
+		if expectationMet(s, expected) {
+			return nil
+		}
+		time.Sleep(3 * time.Second)
 	}
-	return fmt.Errorf("reached timeout while reading status")
+}
+
+func expectationMet(actual status, expected []status) bool {
+	for _, e := range expected {
+		if e == actual {
+			return true
+		}
+	}
+	return false
 }
 
 func (st *Stages) readStatus(ctx context.Context) (status, error) {
