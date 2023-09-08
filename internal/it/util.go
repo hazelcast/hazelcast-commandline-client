8d957cf473ec7d3b64ffb964f223c176feea2d3c
Yuce Tekol <yuce.tekol@hazelcast.com>
2023-09-08 17:44:17 +0300

    Trivial

--- internal/it/util.go
+++ internal/it/util.go
@@ -61,7 +61,6 @@ var defaultDedicatedTestCluster = NewSingletonTestCluster(defaultDedicatedCluste
 	port := NextPort()
 	return rc.startNewCluster(MemberCount(), XMLConfig(defaultDedicatedClusterName, port), port)
 })
-var DefaultDedicatedTestCluster = defaultDedicatedTestCluster
 var defaultViridianTestCluster = NewSingletonTestCluster("not-used", func() TestCluster {
 	return newViridianTestCluster()
 })
