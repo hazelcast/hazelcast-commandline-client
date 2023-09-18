f8742938d1a6d77699f9da5292c57fcde2a0d713
kmetin <kutluhan.metin@hazelcast.com>
2023-09-18 17:03:37 +0300

    delete unnecessary struct

--- base/commands/migration/common.go
+++ base/commands/migration/common.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/hazelcast/hazelcast-go-client"
 	"github.com/hazelcast/hazelcast-go-client/serialization"
@@ -16,16 +15,6 @@ type MigrationStatus struct {
 	Errors               []string `json:"errors"`
 	Report               string   `json:"report"`
 	CompletionPercentage float32  `json:"completionPercentage"`
-}
-
-type Migration struct {
-	Name                 string    `json:"name"`
-	Type                 string    `json:"type"`
-	Status               Status    `json:"status"`
-	StartTimestamp       time.Time `json:"startTimestamp"`
-	EntriesMigrated      int       `json:"entriesMigrated"`
-	TotalEntries         int       `json:"totalEntries"`
-	CompletionPercentage float64   `json:"completionPercentage"`
 }
 
 type UpdateMessage struct {
