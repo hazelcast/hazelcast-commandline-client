de64876a7580b4afd5eaa9c7209164a9af689130
Frantisek Hartman <frant.hartm@gmail.com>
2023-11-03 11:28:24 +0100

    Create new file release_dmt.sh instead of modifying build_dmt.sh

--- build_dmt.sh
+++ build_dmt.sh
@@ -1,6 +1,2 @@
-GOOS=linux GOARCH=amd64 BINARY_NAME=dmt_linux_amd64 MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
-GOOS=linux GOARCH=arm64 BINARY_NAME=dmt_linux_arm64 MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
-GOOS=darwin GOARCH=amd64 BINARY_NAME=dmt_darwin_amd64 MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
-GOOS=darwin GOARCH=arm64 BINARY_NAME=dmt_darwin_arm64 MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
-GOOS=windows GOARCH=amd64 BINARY_NAME=dmt_windows_amd64.exe MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
+BINARY_NAME=dmt MAIN_CMD_HELP='Hazelcast Data Migration Tool' make build-dmt
 
